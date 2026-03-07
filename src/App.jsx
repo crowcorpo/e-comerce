af28942d4ecc24cf434ca53171342d4b2fc6c331
Reda Ghellache <reda.rellache@gmail.com>
2026-03-07 14:11:31 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
+import React, { useState } from "react";
+import { AuthProvider } from './authcontext.jsx';
 
-import "./corpo.css"
-import Corpo from "./corpo";
+import "./navbar.css";
+import Navbar from "./navbar.jsx";
 
-function App(){
+import "./sliderprogram.css";
+import Slider from "./sliderprogram.jsx";
 
-    return(
-        <div>
-            <Corpo/>
-        </div>
+import "./uploadprogram.css";
+import Test2 from "./uploadprogram.jsx";
+
+import "./shoppage2.css";
+import ShopPage2 from "./shoppage2.jsx";
+
+import "./footer.css";
+import Footer from "./footer.jsx";
+
+function App() {
+
+    const [ selectedProduct, setselectedProduct ] = useState(null);
+
+    return (
+        <AuthProvider>
+            <Navbar hideLinks={ !!selectedProduct }/>
+
+            { selectedProduct
+                ? <ShopPage2
+                    productId={ selectedProduct }
+                    onBack={ () => setselectedProduct(null) }
+                  />
+                : <>
+                    <div id="hero">
+                        <Slider/>
+                    </div>
+                    <Test2 onProductClick={ (id) => setselectedProduct(id) }/>
+                  </>
+            }
+
+            <Footer id="footer"/>
+        </AuthProvider>
     );
 }
 
